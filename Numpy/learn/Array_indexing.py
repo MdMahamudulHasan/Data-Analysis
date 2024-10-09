237fb3115cd217fea495ab5ca2b6b50eadce4e03
Md Mahamudul Hasan <126501007+MdMahamudulHasan@users.noreply.github.com>
2024-10-10 02:32:56 +0600

    initial

---
+++ Numpy/learn/Array_indexing.py
@@ -0,0 +1,15 @@
+
+# Array indexing is the same as accessing an array element.
+
+# You can access an array element by referring to its index number.
+
+
+# -------------  Access 1D Arrays Element---------------------------
+
+import numpy as np
+arr = np.array([1, 2, 3, 4])
+print(arr[0])
+print(arr[2])
+print(arr[1:2])
+print(arr[1] + arr[2])
+
